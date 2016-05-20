f502d34a18f5f6eaa6749efface513aaebe0cde9
Yuting Liu <yuting9@gmail.com>
2016-05-20 14:52:43 -0400

    Main Menu
    Began work on the Main Menu

--- src/bin/PrisonBreak.java
+++ src/bin/PrisonBreak.java
@@ -1,9 +1,11 @@
+package bin;
+import bin.mainMenu;
 
 public class PrisonBreak {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		mainMenu game = new mainMenu();
 	}
 
 }
