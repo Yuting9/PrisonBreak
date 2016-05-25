ef7800e5925b285c49512b1591fae10c5f5493ab
aunikdc <aunikd@rogers.com>
2016-05-24 22:51:34 -0400

    changed button layout

--- src/bin/mainMenu.java
+++ src/bin/mainMenu.java
@@ -2,46 +2,49 @@ package bin;
 import javax.swing.*;
 import java.awt.*;
 
-public class mainMenu {
-	JFrame main = new JFrame("Prison Break - Main Menu");
-	
-	public mainMenu(){
-		main.setBounds(0,0,600,500);
-		main.getContentPane().setLayout(new BorderLayout(0, 0));
-
-		JPanel namePnl = new JPanel();
-		main.getContentPane().add(namePnl, BorderLayout.NORTH);
-
-		JPanel centerPnl = new JPanel();
-		main.getContentPane().add(centerPnl, BorderLayout.CENTER);
-		
-		JPanel btnPnl = new JPanel();
-		main.getContentPane().add(btnPnl, BorderLayout.SOUTH);
-		btnPnl.setLayout(new BoxLayout(btnPnl, BoxLayout.Y_AXIS));
-		
-		JButton btnNewGame = new JButton("New Game");
-		btnNewGame.setPreferredSize(new Dimension());
-		btnNewGame.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnPnl.add(btnNewGame);
-		
-		JButton btnHowToPlay = new JButton("How to Play");
-		btnHowToPlay.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnPnl.add(btnHowToPlay);
-		
-		JButton btnLeaderboards = new JButton("Leaderboards");
-		btnLeaderboards.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnPnl.add(btnLeaderboards);
-		
-		JLabel lblTitle = new JLabel("On The Run");
-		lblTitle.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 38));
-		namePnl.add(lblTitle);
-		
-		JLabel lblNewLabel = new JLabel("");
-		ImageIcon image = new ImageIcon(getClass().getResource("/mainMenuIcon.jpg"));
-		lblNewLabel.setIcon(image);
-		centerPnl.add(lblNewLabel);
-		
-		main.setResizable(false);
-		main.setVisible(true);
-	}
+public class mainMenu 
+{
+  JFrame main = new JFrame("Prison Break - Main Menu");
+  
+  public mainMenu()
+  {
+    main.setBounds(0,0,600,500);
+    main.getContentPane().setLayout(new BorderLayout(0, 0));
+    
+    JPanel namePnl = new JPanel();
+    main.getContentPane().add(namePnl, BorderLayout.NORTH);
+    
+    JPanel centerPnl = new JPanel();
+    main.getContentPane().add(centerPnl, BorderLayout.CENTER);
+    
+    JPanel btnPnl = new JPanel();
+    main.getContentPane().add(btnPnl, BorderLayout.SOUTH);
+    btnPnl.setLayout(new GridLayout(1, 3));
+    btnPnl.setPreferredSize(new Dimension(0, 40));
+    
+    JButton btnNewGame = new JButton("New Game");
+    btnNewGame.setPreferredSize(new Dimension());
+    //btnNewGame.setAlignmentX(Component.CENTER_ALIGNMENT);
+    btnPnl.add(btnNewGame);
+   
+    JButton btnHowToPlay = new JButton("How to Play");
+    //btnHowToPlay.setAlignmentX(Component.CENTER_ALIGNMENT);
+    btnPnl.add(btnHowToPlay);
+    
+    JButton btnLeaderboards = new JButton("Leaderboards");
+    btnLeaderboards.setAlignmentX(Component.CENTER_ALIGNMENT);
+    btnPnl.add(btnLeaderboards);
+    
+    JLabel lblTitle = new JLabel("On The Run");
+    lblTitle.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 38));
+    namePnl.add(lblTitle);
+    
+    JLabel lblNewLabel = new JLabel("");
+    //ImageIcon image = new ImageIcon(getClass().getResource("mainMenuIcon.jpg"));
+    //lblNewLabel.setIcon(image);
+    centerPnl.add(lblNewLabel);
+    
+    main.setResizable(false);
+    main.setVisible(true);
+  }
 }
