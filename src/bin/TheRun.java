ed70f6f787f37e46019cd7fb09ed2adbddc052cf
Yuting Liu <yuting9@gmail.com>
2016-06-01 12:34:23 -0400

    Actual Game File Part 1

---
+++ src/bin/TheRun.java
@@ -0,0 +1,21 @@
+package bin;
+
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TheRun {
+	JFrame game = new JFrame("On the Run");
+	
+}
