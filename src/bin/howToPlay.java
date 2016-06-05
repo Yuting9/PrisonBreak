508bd65d772fd7311dd68965f23a4ff0c14f1f26
Yuting Liu <yuting9@gmail.com>
2016-06-05 13:42:06 -0400

    Continued Overhaul

--- src/bin/howToPlay.java
+++ src/bin/howToPlay.java
@@ -37,7 +37,7 @@ public class howToPlay
     pnlObjective.setLayout(new GridLayout(2, 1) );
     JLabel lblObjective = new JLabel("The objective of the game is to catch the prisoner.");
     JLabel prisonerImg = new JLabel();
-    ImageIcon objectiveIcon = new ImageIcon(PrisonBreak.class.getResource("/img/prisoner1.png")); 
+    ImageIcon objectiveIcon = new ImageIcon(PrisonBreak.class.getResource("/prisoner1.png")); 
     prisonerImg.setIcon(objectiveIcon);
     pnlObjective.add(lblObjective);
     pnlObjective.add(prisonerImg);
@@ -47,7 +47,7 @@ public class howToPlay
     pnlJump.setLayout(new GridLayout(2, 1) );
     JLabel lblJump = new JLabel("You will use the spacebar to jump.");
     JLabel jumpImg = new JLabel();
-    ImageIcon jumpIcon = new ImageIcon(PrisonBreak.class.getResource("/img/prisoner1.png")); 
+    ImageIcon jumpIcon = new ImageIcon(PrisonBreak.class.getResource("/prisoner1.png")); 
     jumpImg.setIcon(jumpIcon);
     pnlJump.add(lblJump);
     pnlJump.add(jumpImg);
@@ -57,7 +57,7 @@ public class howToPlay
     pnlDonut.setLayout(new GridLayout(2, 1) );
     JLabel lblDonut = new JLabel("Donuts will slow you down.");
     JLabel donutImg = new JLabel();
-    ImageIcon donutIcon = new ImageIcon(PrisonBreak.class.getResource("/img/prisoner1.png"));
+    ImageIcon donutIcon = new ImageIcon(PrisonBreak.class.getResource("/prisoner1.png"));
     donutImg.setIcon(donutIcon);
     pnlDonut.add(lblDonut);
     pnlDonut.add(donutImg);
@@ -67,7 +67,7 @@ public class howToPlay
     pnlCoffee.setLayout(new GridLayout(2, 1) );
     JLabel lblCoffee = new JLabel("Coffee will speed you up.");
     JLabel coffeeImg = new JLabel();
-    ImageIcon coffeeIcon = new ImageIcon(PrisonBreak.class.getResource("/img/coffee.png"));
+    ImageIcon coffeeIcon = new ImageIcon(PrisonBreak.class.getResource("/coffee.png"));
     coffeeImg.setIcon(coffeeIcon);
     pnlCoffee.add(lblCoffee);
     pnlCoffee.add(coffeeImg);
