e9de22d6df1ba6d77238e5bb4bf67cddbf6892de
aunikdc <aunikd@rogers.com>
2016-06-05 14:51:14 -0400

    added buildings

--- src/bin/TheRun.java
+++ src/bin/TheRun.java
@@ -5,29 +5,35 @@ import java.awt.event.*;
 
 public class TheRun extends JPanel implements ActionListener
 {
-	JFrame f = new JFrame();
-	ImageIcon anima;
-	int i = 0, d = 500;
+ JFrame f = new JFrame();
+ ImageIcon anima;
+ int i = 0, d = 500;
     JLabel holder = new JLabel();
+    JLabel background = new JLabel();
     Timer clock = new Timer(40, this);
+    ImageIcon test;
     
     public void Window()
     {
-    	f.setResizable(false);
+     f.setResizable(false);
         add(holder);
         clock.start();
         f.setSize(600,450);
         f.getContentPane().add(this);
         f.setVisible(true);
+        add(background);
     }
     
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        
+        test = new ImageIcon(PrisonBreak.class.getResource("/img/UrbanBuilding1.png"));
+        this.remove(background);
+        background = new JLabel(test);
+        add(background);
         clock.setDelay(d);
         if(d>=42)
-        	d-=(d/10);
+         d-=(d/10);
         anima = Character.getPrison(i);
         this.remove(holder);
         holder = new JLabel(anima);
