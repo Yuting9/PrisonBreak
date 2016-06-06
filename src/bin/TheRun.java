7633e4b2cc59ad8880ebaafabbce79b58c6b3f51
aunikdc <aunikd@rogers.com>
2016-06-05 23:27:02 -0400

    testing background

--- src/bin/TheRun.java
+++ src/bin/TheRun.java
@@ -2,16 +2,18 @@ package bin;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.Graphics;
 
 public class TheRun extends JPanel implements ActionListener
 {
  JFrame f = new JFrame();
  ImageIcon anima;
- int i = 0, d = 500;
+ int i = 0, d = 500, x = 300;
     JLabel holder = new JLabel();
     JLabel background = new JLabel();
     Timer clock = new Timer(40, this);
-    ImageIcon test;
+    ImageIcon test = new ImageIcon(PrisonBreak.class.getResource("/img/UrbanBuilding" + ((int)(Math.random()*2)+1) + ".png"));
+    
     
     public void Window()
     {
@@ -21,16 +23,22 @@ public class TheRun extends JPanel implements ActionListener
         f.setSize(600,450);
         f.getContentPane().add(this);
         f.setVisible(true);
-        add(background);
+       
+        background = new JLabel(test);
+        add(background);       
     }
     
+        public void paintComponent(Graphics g)
+        {
+          x--;
+          super.paintComponent(g);
+          test.paintIcon(this,g,x,0);
+          repaint();
+        }
+        
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        test = new ImageIcon(PrisonBreak.class.getResource("/img/UrbanBuilding1.png"));
-        this.remove(background);
-        background = new JLabel(test);
-        add(background);
         clock.setDelay(d);
         if(d>=42)
          d-=(d/10);
