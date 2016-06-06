74d9a7fae0f4f3cfb3b179a1a677bd8e23e2ef83
Yuting Liu <yuting9@gmail.com>
2016-06-06 10:07:25 -0400

    Revert "Minor Edits to Formatting"
    This reverts commit 2b83beca50651a2265ed708b0b4bfe2d41744127.
    
    # Conflicts:
    #	src/bin/TheRun.java
    #	src/bin/leaderboard.java

--- src/bin/TheRun.java
+++ src/bin/TheRun.java
@@ -3,14 +3,17 @@ package bin;
 import javax.swing.*;
 import java.awt.event.*;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import java.awt.BorderLayout;
+=======
+>>>>>>> parent of 2b83bec... Minor Edits to Formatting
 
 public class TheRun extends JPanel implements ActionListener
 {
-	
-	JFrame frame = new JFrame();
-	ImageIcon prison;
+	JFrame f = new JFrame();
+	ImageIcon anima;
 	int i = 0, d = 500;
+<<<<<<< HEAD
 	int pX = 100, pY = 100;
 =======
 import java.awt.Graphics;
@@ -20,9 +23,12 @@ public class TheRun extends JPanel implements ActionListener
  JFrame f = new JFrame();
  ImageIcon anima;
  int i = 0, d = 500, x = 300;
+=======
+>>>>>>> parent of 2b83bec... Minor Edits to Formatting
     JLabel holder = new JLabel();
     JLabel background = new JLabel();
     Timer clock = new Timer(40, this);
+<<<<<<< HEAD
 	public TheRun() {
 		frame.setResizable(false);
 		frame.setSize(600,450);
@@ -38,14 +44,23 @@ public class TheRun extends JPanel implements ActionListener
     public void Window()
     {
      f.setResizable(false);
+=======
+    
+    public void Window()
+    {
+    	f.setResizable(false);
+>>>>>>> parent of 2b83bec... Minor Edits to Formatting
         add(holder);
         clock.start();
         f.setSize(600,450);
         f.getContentPane().add(this);
         f.setVisible(true);
+<<<<<<< HEAD
        
         background = new JLabel(test);
         add(background);       
+=======
+>>>>>>> parent of 2b83bec... Minor Edits to Formatting
     }
     
         public void paintComponent(Graphics g)
@@ -62,11 +77,14 @@ public class TheRun extends JPanel implements ActionListener
         clock.setDelay(d);
         if(d>=42)
         	d-=(d/10);
+<<<<<<< HEAD
         prison = Character.getPrison(i);
          d-=(d/10);
+=======
+>>>>>>> parent of 2b83bec... Minor Edits to Formatting
         anima = Character.getPrison(i);
         this.remove(holder);
-        holder = new JLabel(prison);
+        holder = new JLabel(anima);
         add(holder);
         i++;
         if (i == 6 )
@@ -74,14 +92,5 @@ public class TheRun extends JPanel implements ActionListener
             i = 0;
         }
         revalidate();
-    }
-    
-    class DrawPanel extends JPanel{
-
-		public void add(ImageIcon img, int x, int y) {
-			// TODO Auto-generated method stub
-			
-		}
-    	
     }
 }
--- src/bin/leaderboard.java
+++ src/bin/leaderboard.java
@@ -15,17 +15,16 @@ import javax.swing.JPanel;
 import javax.swing.BoxLayout;
 import javax.swing.SwingConstants;
 import java.awt.FlowLayout;
-import java.io.*;
 
 public class leaderboard 
 {
-	FileInputStream in = null;
-	final JFrame lead = new JFrame("On the Run - Leaderboards");
-	leaderboard(final JFrame main){
-		lead.getContentPane().setLayout(new BoxLayout(lead.getContentPane(), BoxLayout.X_AXIS));
-		lead.setVisible(true);
-		lead.setBounds(300,100,600,450);
+ final JFrame lead = new JFrame("On the Run - Leaderboards");
+ leaderboard(final JFrame main){
+  lead.getContentPane().setLayout(new BoxLayout(lead.getContentPane(), BoxLayout.X_AXIS));
+  lead.setVisible(true);
+  lead.setBounds(300,100,600,450);
   
+<<<<<<< HEAD
 		try {
 			in = new FileInputStream("lead.txt");
 			//in.read();
@@ -33,30 +32,31 @@ public class leaderboard
 			File leadFile = new File("/lead.txt");
 			e.printStackTrace();
 		}
+=======
+  JPanel pnlTime = new JPanel();
+  lead.getContentPane().add(pnlTime);
+  pnlTime.setLayout(new BorderLayout(0, 0));
+>>>>>>> parent of 2b83bec... Minor Edits to Formatting
   
-		JPanel pnlTime = new JPanel();
-		lead.getContentPane().add(pnlTime);
-		pnlTime.setLayout(new BorderLayout(0, 0));
+  JLabel lblHiScore = new JLabel("Leaderboards - High Score");
+  lblHiScore.setHorizontalAlignment(SwingConstants.CENTER);
+  pnlTime.add(lblHiScore, BorderLayout.NORTH);
   
-		JLabel lblHiScore = new JLabel("Leaderboards - High Score");
-		lblHiScore.setHorizontalAlignment(SwingConstants.CENTER);
-		pnlTime.add(lblHiScore, BorderLayout.NORTH);
+  JLabel lblScores = new JLabel("");
+  pnlTime.add(lblScores, BorderLayout.CENTER);
   
-		JLabel lblScores = new JLabel("");
-		pnlTime.add(lblScores, BorderLayout.CENTER);
+  JPanel pnlBtn = new JPanel();
+  pnlTime.add(pnlBtn, BorderLayout.SOUTH);
+  pnlBtn.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
   
-		JPanel pnlBtn = new JPanel();
-		pnlTime.add(pnlBtn, BorderLayout.SOUTH);
-		pnlBtn.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+  JButton btnRtn = new JButton("Return to Menu");
+  btnRtn.addActionListener(new ActionListener() {
+   public void actionPerformed(ActionEvent e) {
+    main.setVisible(true);
+    lead.dispose();
+   }
+  });
+  pnlBtn.add(btnRtn);
   
-		JButton btnRtn = new JButton("Return to Menu");
-		btnRtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				main.setVisible(true);
-				lead.dispose();
-			}
-		});
-		pnlBtn.add(btnRtn);
-  
-	}
+ }
 }
